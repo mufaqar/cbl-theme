408aa8f7c83785dda037963ab6dcc4cb46a16f40
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-11-29 17:26:05 +0500

    update

--- single-providers.php
+++ single-providers.php
@@ -82,11 +82,14 @@
     <div class="container mx-auto flex justify-center items-center md:text-4xl text-xl font-bold uppercase text-white">
         <div class="grid items-center md:justify-end justify-center"><?php if($order_online){ echo "Simply Click Here And "; }else{ echo "Call Now to Order"; } ?></div>
         <div class="items-center justify-start flex gap-3">
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="ml-5 md:text-4xl text-2xl font-normal" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"
-                ></path>
-            </svg>
+            <?php
+                if(!$order_online){ ?>
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="ml-5 md:text-4xl text-2xl font-normal" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"
+                    ></path>
+                </svg>
+            <?php } ?>
             <a href="tel:<?php echo $phone ?>">
                 <?php if($order_online){ echo "Order Online"; }else{ echo $phone; } ?> 
             </a>
