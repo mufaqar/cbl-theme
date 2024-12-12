c6cd7b196686475a57e0af7cb083635960bc6fd9
mufaqar <mufaqar@gmail.com>
2024-12-12 18:47:24 +0500

    single

--- single-providers.php
+++ single-providers.php
@@ -21,6 +21,15 @@
     $cons = get_field( "cons" );
     $order_online = get_field( "order_online" );
     $disclambers = get_field( "disclambers" );
+
+    function order_online()
+    { ?>
+        <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" 
+        href="<?php echo $phone ? 'tel:' . $phone : $order_online; ?>">                               
+         <?php echo $phone ?  $phone : "Order Online"; ?>
+    </a>
+    <?php
+    }
     
 
 ?>
@@ -81,17 +90,15 @@
     <div class="container mx-auto flex justify-center items-center md:text-4xl text-xl font-bold uppercase text-white">
         <div class="grid items-center md:justify-end justify-center"><?php if($order_online){ echo "Simply Click And"; }else{ echo "Call Now to Order"; } ?> </div>
         <div class="items-center justify-start flex gap-3">
-            <?php
-                if(!$order_online){ ?>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="ml-5 md:text-4xl text-2xl font-normal" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"
-                    ></path>
-                </svg>
-            <?php } ?>
-            <a href="tel:<?php echo $phone ?>" class="ml-1.5">
-                <?php if($order_online){ echo " Order Online"; }else{ echo $phone; } ?> 
-            </a>
+        <?php if (!$order_online): ?>
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="ml-5 md:text-4xl text-2xl font-normal" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path>
+            </svg>
+            <a href="tel:<?php echo $phone ?>" class="ml-1.5"><?php echo $phone; ?></a>
+        <?php endif; ?>
+        <a href="<?php echo $order_online ?: '#'; ?>" class="ml-1.5"><?php echo $order_online ? 'Order Online' : ''; ?></a>
+
+                
         </div>
     </div>
 </section>
@@ -150,11 +157,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                                <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                        <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
@@ -219,11 +222,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] overflow-hidden items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                                <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                      <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
@@ -301,11 +300,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] overflow-hidden items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                            <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                    <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
@@ -383,11 +378,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] overflow-hidden items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                            <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                        <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
@@ -465,11 +456,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] overflow-hidden items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                            <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                      <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
@@ -559,11 +546,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] overflow-hidden items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                            <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                    <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
@@ -626,11 +609,7 @@
                                         </div>
                                     </div>
                                     <div class="w-full grid justify-center md:p-5 p-2 md:h-auto h-[120px] overflow-hidden items-center">
-                                        <div>
-                                            <a class="md:text-base text-[9px] font-medium text-white bg-[#ef9831] hover:bg-[#215690] md:px-3 px-[5px] py-1.5 rounded-3xl block w-[90px] md:w-[140px] text-center mx-auto" href="tel:<?php echo $phone ?>">
-                                            <?php echo $phone ?>
-                                            </a>
-                                        </div>
+                                    <div> <?php  echo order_online() ?> </div>
                                     </div>
                                 </div>
                             <?php endwhile; ?>
