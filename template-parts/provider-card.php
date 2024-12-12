2249d378489c81fd707fe6720d7ed4bf17c6c3ed
mufaqar <mufaqar@gmail.com>
2024-12-12 17:31:39 +0500

    privider card

--- template-parts/provider-card.php
+++ template-parts/provider-card.php
@@ -7,42 +7,38 @@ $servicesInfo = get_field('services_info');
 $type = get_query_var('type');
 
 
-$isSpeed = $type === "tv";
-if($isSpeed){
-    $speed =  $servicesInfo["tv_services"]["speed"];
-    $features_items = explode(',', $servicesInfo["tv_services"]["features"]);
-    $price = $servicesInfo["tv_services"]["price"];
-}else{
-    $speed =  $servicesInfo["internet_services"]["speed"];
-    $features_items = explode(',', $servicesInfo["internet_services"]["features"]);
-    $price = $servicesInfo["internet_services"]["price"];
+
+
+$servicesInfo = get_field('services_info');
+
+
+if ($type == 'internet') {
+    $services = $servicesInfo["internet_services"];
+} elseif ($type == 'tv') {
+    $services = $servicesInfo["tv_services"];
+    $channels = $services['channels'];
+} elseif ($type == 'landline') {
+    $services = $servicesInfo["landline_services"];
+} else {
+    $services = $servicesInfo["home_security_services"];
 }
 
-$phone;
-$view_link;
-$deals;
+//var_dump($services);
 
-if($type === "internet"){
-    $phone =  $servicesInfo["internet_services"]["phone"];
-    $view_link =  $servicesInfo["internet_services"]["view_more"];
-    $deals = $servicesInfo["internet_services"]["deals"];
-}
-if($type === "tv"){
-    $phone =  $servicesInfo["tv_services"]["phone"];
-    $view_link =  $servicesInfo["tv_services"]["view_more"];
-    $deals = $servicesInfo["tv_services"]["deals"];
-}
-if($type === "landline"){
-    $phone =  $servicesInfo["landline_services"]["phone"];
-    $view_link =  $servicesInfo["landline_services"]["view_more"];
-    $deals = $servicesInfo["landline_services"]["deals"];
-}
-if($type === "home-security"){
-    $phone =  $servicesInfo["home_security_services"]["phone"];
-    $view_link =  $servicesInfo["home_security_services"]["view_more"];
-    $deals = $servicesInfo["home_security_services"]["deals"];
+// var_dump($services);
+$price =  $services['price'];
+$speed =  $services['speed'];
+$phone =  $services['phone'];
+$view_link =  $services['view_more'];
+$deals =  $services['deals'];
+$features = $services['features'];
+$features_items  = explode(',', $features); 
 
-}
+
+
+
+
+
 
 
 ?>
@@ -51,44 +47,28 @@ if($type === "home-security"){
 <div class="grid gap-7 mb-7">
     <div class="w-full h-auto shadow-xl border rounded-t-md rounded-b-md flex flex-col">
         <div class="md:w-full min-w-fit bg-[#215690] flex justify-between items-center">
-            <h2 class="text-base font-bold text-center text-white p-5"><span> <?php echo $index ?> </span>- <?php the_title()?></h2>
-            <h2 class="text-base font-bold text-center text-white p-5">
-                <?php
-                    if($deals){
-                        echo $deals;
-                    }
-                ?>
-            </h2>
+            <h2 class="text-base font-bold text-center text-white p-5"><span> <?php echo $index ?> </span>-
+                <?php the_title()?></h2>
+            <h2 class="text-base font-bold text-center text-white p-5"> <?php  if($deals){ echo $deals; }?></h2>
         </div>
-        <div class="md:w-full w-full grid grid-cols-1 dtable md:grid-cols-5 flex-col">
+        <div class="md:w-full w-full grid grid-cols-1 dtable  <?php echo $type == 'home-security' ? 'md:grid-cols-4' : 'md:grid-cols-5'; ?> flex-col ">
             <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
                 <a target="_blank" href="<?php the_permalink()?>">
-                    <img
-                        alt="Feature Image"
-                        loading="lazy"
-                        width="140"
-                        height="50"
-                        decoding="async"
-                        data-nimg="1"
-                        src="<?php echo $logoUrl ?>"
-                        style="color: transparent;"
-                    />
+                    <img alt="Feature Image" loading="lazy" width="140" height="50" decoding="async" data-nimg="1"
+                        src="<?php echo $logoUrl ?>" style="color: transparent;" />
                 </a>
             </div>
-            <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
-                <div class="text-center">
-                    <?php if ($isSpeed): ?>
-                        <p class="tch">Channels are</p>
-                    <?php else: ?>
-                        <p class="tch">Speed from</p>
-                    <?php endif; ?>
-                    <p class="tcd"><?php echo $speed  ?> 
-                        <?php if (!$isSpeed): ?>
-                            <span class="tch">Mbps</span>
-                        <?php endif; ?>
-                    </p>
+            <?php if ($type != 'home-security') : ?>
+                <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
+                    <div class="text-center">
+                        <p class="tch"><?php echo $type == 'tv' ? 'Channels' : 'Speed from'; ?></p>
+                        <span class="tcd">
+                            <?php echo $type == 'tv' ? $channels : $speed . ' Mbps'; ?>
+                        </span>
+                    </div>
                 </div>
-            </div>
+            <?php endif; ?>
+
             <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5 px-3">
                 <?php                  
                    echo '<ul class="grid items-center justify-center">';                   
@@ -106,7 +86,8 @@ if($type === "home-security"){
             <div class="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
                 <div>
                     <p class="tch">Pricing starts from</p>
-                    <p class="tcd"><span class="font-extrabold text-[#215690] font-[Roboto] text-xl"> $<?php echo $price; ?> </span> /mo.</p>
+                    <p class="tcd"><span class="font-extrabold text-[#215690] font-[Roboto] text-xl">
+                            $<?php echo $price; ?> </span> /mo.</p>
                 </div>
             </div>
             <?php echo render_provider_buttons($phone, $view_link); ?>
