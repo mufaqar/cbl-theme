36b14eb7c565e9b7aa2befc922d8cc6472473b9f
mufaqar <mufaqar@gmail.com>
2025-01-19 21:16:35 +0500

    updated

--- template-parts/section/text-providers.php
+++ template-parts/section/text-providers.php
@@ -68,7 +68,7 @@
                 best landline phone service. That may include any combination of:
             </p>
             <ul class="PClass">
-                <li><strong>Internet Requirements:</strong> Do the landline home phone service providers in Glendale, CA, require you
+                <li><strong>Internet Requirements:</strong> Do the landline home phone service providers in <?php echo $city?>, <?php echo $state?> require you
                     to have internet access to install or use the lines being offered? </li>
                     <li><strong>Hidden Fees & Taxes:</strong>  Does signing up for the landline home service providers mean paying hidden
                     fees that increase over time or are there any local taxes not worked into the monthly price
