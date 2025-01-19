98345b2d895e948b62bdf88e4c669c1a9dc7e65b
mufaqar <mufaqar@gmail.com>
2025-01-19 21:03:23 +0500

    text

--- template-parts/provider-card.php
+++ template-parts/provider-card.php
@@ -75,7 +75,7 @@ $features_items  = explode(',', $features);
                     <p class="tcd"><span class="font-extrabold text-[#215690] font-[Roboto] text-xl">
                             $<?php echo $price; ?> </span> /mo.</p>
                             <?php if (in_array($type, ['landline'])) : ?>
-                            <p class="tch">
+                            <p class="tch text-sm">
                             <?php echo $pacakge_info; ?></p>
                             <?php endif; ?>
                 </div>
