71f93d9edf638a6d3e2224e33407d1f3c6fe910f
mufaqar <mufaqar@gmail.com>
2025-01-20 14:52:28 +0500

    Favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 
