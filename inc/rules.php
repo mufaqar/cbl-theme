27ba070f55b1d6f4b3d0f9ded63a3bcdd86c4c46
mufaqar <mufaqar@gmail.com>
2025-02-02 20:18:31 +0500

    SEO

--- inc/rules.php
+++ inc/rules.php
@@ -486,7 +486,6 @@ add_action('template_redirect', function () {
 
 
 add_filter('wpseo_robots', 'yoast_no_home_noindex', 999);
-
 function yoast_no_home_noindex($string) {
     $string = "index,follow";
     if (is_singular('area_zone')) {
